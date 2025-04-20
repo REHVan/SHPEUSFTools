d89614d19f9d1888e59f452ca977a9cbbc4ebc79
REHVan <106537160+REHVan@users.noreply.github.com>
2025-04-20 15:38:55 -0400

    fix use right port

--- server.js
+++ server.js
@@ -667,7 +667,9 @@ app.delete('/delete_contact/:id', async (req, res) => {
 });
 
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('Server is running on port 5000');
 });
 
