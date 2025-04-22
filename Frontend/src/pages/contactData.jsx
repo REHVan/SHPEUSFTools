a6ec3fd26d538afa1ddf382eea15c2ea97d6f8bb
REHVan <106537160+REHVan@users.noreply.github.com>
2025-04-22 15:06:16 -0400

    Add in UID

--- Frontend/src/pages/contactData.jsx
+++ Frontend/src/pages/contactData.jsx
@@ -48,6 +48,26 @@ function ContactData() {
   }, []);
 
   const handleAddContact = async (e) => {
+
+    var UID = 0;
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+    
+      // Check if the key starts with 'firebase:authUser:'
+      if (key.startsWith('firebase:authUser:')) {
+        // Get the user data stored under this key
+        const firebaseUser = sessionStorage.getItem(key);
+    
+        // Parse the JSON and extract the UID
+        if (firebaseUser) {
+          const parsedUser = JSON.parse(firebaseUser);
+          const uid = parsedUser.uid; 
+          UID =uid;
+          break; // Exit loop after finding the correct key
+        }
+      }
+    }
+    
     e.preventDefault();
     try {
       const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}add_contact?uid=${UID}`, {
