8deb7b62629742925c2b04ed939f110e3c090bf0
REHVan <106537160+REHVan@users.noreply.github.com>
2025-04-22 21:33:21 -0400

    Undo navigation

--- Frontend/src/components/UserNavBar.jsx
+++ Frontend/src/components/UserNavBar.jsx
@@ -1,27 +1,48 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function UserNavBar() {
+//     return (
+//         <nav className="bg-black p-4 h-screen w-48 fixed shadow-lg rounded-r-lg">
+//             <div className="flex flex-col items-start">
+//                 <ul className="space-y-4">
+//                     <li>
+//                         <Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
+//                             Send Email
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
+//                             Contact Data
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
+//                             Email Template
+//                         </Link>
+//                     </li>
+
+//                 </ul>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default UserNavBar;
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoutButton from '../components/LogoutButton';
 
 function UserNavBar() {
     return (
-        <nav className="bg-black p-4 h-screen w-48 fixed shadow-lg rounded-r-lg">
-            <div className="flex flex-col items-start">
-                <ul className="space-y-4">
-                    <li>
-                        <Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
-                            Send Email
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
-                            Contact Data
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md block text-sm">
-                            Email Template
-                        </Link>
-                    </li>
-
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <ul className="flex space-x-4">
+                    <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Send Email</Link></li>
+                    <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Contact Data</Link></li>
+                    <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Email Template</Link></li>
                 </ul>
             </div>
         </nav>
@@ -29,42 +50,3 @@ function UserNavBar() {
 }
 
 export default UserNavBar;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// function UserNavBar() {
-//     return (
-//         <nav className="bg-gray-800 p-4 h-screen w-64 fixed">
-//             <div className="container mx-auto flex flex-col items-start">
-//                 <ul className="space-y-4">
-//                     <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Send Email</Link></li>
-//                     <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Contact Data</Link></li>
-//                     <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Email Template</Link></li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// }
-
-// export default UserNavBar;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import LogoutButton from '../components/LogoutButton';
-
-// function UserNavBar() {
-//     return (
-//         <nav className="bg-gray-800 p-4">
-//             <div className="container mx-auto flex justify-between items-center">
-//                 <ul className="flex space-x-4">
-//                     <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Send Email</Link></li>
-//                     <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Contact Data</Link></li>
-//                     <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Email Template</Link></li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// }
-
-// export default UserNavBar;
