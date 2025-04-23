2a5add8e204ade1f9765585eadf2a8147afd9069
REHVan <106537160+REHVan@users.noreply.github.com>
2025-04-22 21:24:06 -0400

    FIx navigatipn

--- Frontend/src/components/UserNavBar.jsx
+++ Frontend/src/components/UserNavBar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoutButton from '../components/LogoutButton';
 
 function UserNavBar() {
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="bg-gray-800 p-4 h-screen w-64 fixed">
             <div className="container mx-auto flex flex-col items-start">
                 <ul className="space-y-4">
-                    <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Send Email</Link></li>
-                    <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Contact Data</Link></li>
-                    <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Email Template</Link></li>
+                    <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Send Email</Link></li>
+                    <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Contact Data</Link></li>
+                    <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded block">Email Template</Link></li>
                 </ul>
             </div>
         </nav>
