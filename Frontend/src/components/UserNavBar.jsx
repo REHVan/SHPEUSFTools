55bc62d1933374b4ca622df29680d5448527613d
REHVan <106537160+REHVan@users.noreply.github.com>
2025-04-22 21:22:10 -0400

    FIx navigation

--- Frontend/src/components/UserNavBar.jsx
+++ Frontend/src/components/UserNavBar.jsx
@@ -5,8 +5,8 @@ import LogoutButton from '../components/LogoutButton';
 function UserNavBar() {
     return (
         <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <ul className="flex space-x-4">
+            <div className="container mx-auto flex flex-col items-start">
+                <ul className="space-y-4">
                     <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Send Email</Link></li>
                     <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Contact Data</Link></li>
                     <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Email Template</Link></li>
@@ -17,3 +17,23 @@ function UserNavBar() {
 }
 
 export default UserNavBar;
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import LogoutButton from '../components/LogoutButton';
+
+// function UserNavBar() {
+//     return (
+//         <nav className="bg-gray-800 p-4">
+//             <div className="container mx-auto flex justify-between items-center">
+//                 <ul className="flex space-x-4">
+//                     <li><Link to="/external" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Send Email</Link></li>
+//                     <li><Link to="/contactdata" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Contact Data</Link></li>
+//                     <li><Link to="/template" className="text-white hover:bg-gray-700 px-3 py-2 rounded">Email Template</Link></li>
+//                 </ul>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default UserNavBar;
